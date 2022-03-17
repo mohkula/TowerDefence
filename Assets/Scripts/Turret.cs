00727caee5ebe46c20b303c93f431637afb6d249
Mohkula <matias.himanen@helsinki.fi>
2022-03-17 10:41:00 +0200

    added shooting

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -7,13 +7,24 @@ public class Turret : MonoBehaviour
 
     private Transform target;
 
+[Header("Attributes")]
+ public float fireRate = 1f;
+    private float fireCountdown = 0f;
     public float range = 15f;
 
+
+[Header("Unity setup Fields")]
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
 
     public float turnSpeed = 10f;
+
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+   
 
 
 
@@ -59,6 +70,25 @@ public class Turret : MonoBehaviour
     Quaternion lookRotation = Quaternion.LookRotation(dir);
     Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
     partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+    if (fireCountdown <= 0f){
+
+        Shoot();
+        fireCountdown = 1f / fireRate;
+    }
+
+    fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot (){
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+
+        if(bullet != null){
+
+            bullet.Seek(target);
+        }
 
     }
 
